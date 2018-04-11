e8759c96852a70d93f544d270fc78d5487264b87
Steven Grimaldo <sgrimaldo@gsandf.com>
2018-04-11 08:48:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var sections = document.querySelectorAll('section');
+var elCushion = 0.5;
+var offsetTop = 0;
+var offsetBottom = 0;
+
+var inView = function(el) {
+  // Set container positions
+  var cTop = window.pageYOffset;
+  var cBottom = window.innerHeight - offsetBottom;
+
+  // Get elements height + set visible offset
+  var elHeight = el.getBoundingClientRect().height;
+  var elOffset = elHeight * elCushion;
+
+  // Set the elements top and bottom points
+  var elTop = el.getBoundingClientRect().top;
+  var elBottom = elTop + elHeight;
+
+  // Get elements positions minus offsets
+  var eTop = elTop + elOffset;
+  var eBottom = elTop + elHeight - offsetTop;
+
+  return (eTop <= cBottom && eBottom >= elOffset);
+};
+
+var checkVisibility = function() {
+	for (var section of sections) {
+	  if (inView(section)) {
+			section.classList.add('in-view');
+		} else {
+			section.classList.remove('in-view');
+		}
+	}
+};
+
+window.addEventListener('load', checkVisibility, false);
+window.addEventListener('scroll', checkVisibility, false);
