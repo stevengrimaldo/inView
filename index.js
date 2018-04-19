6088af0a5c199d538ef9affae91c9fc92b3be372
Steven Grimaldo <sgrimaldo@gsandf.com>
2018-04-18 20:37:44 -0500

    updating vars

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var sections = document.querySelectorAll('section');
-var elCushion = 0.5;
-var offsetTop = 0;
-var offsetBottom = 0;
+const sections = document.querySelectorAll('section');
+const elCushion = 0.5;
+const offsetTop = 0;
+const offsetBottom = 0;
 
-var inView = function(el) {
+const inView = el => {
   // Set container positions
-  var cTop = window.pageYOffset;
-  var cBottom = window.innerHeight - offsetBottom;
+  const cTop = window.pageYOffset;
+  const cBottom = window.innerHeight - offsetBottom;
 
   // Get elements height + set visible offset
-  var elHeight = el.getBoundingClientRect().height;
-  var elOffset = elHeight * elCushion;
+  const elHeight = el.getBoundingClientRect().height;
+  const elOffset = elHeight * elCushion;
 
   // Set the elements top and bottom points
-  var elTop = el.getBoundingClientRect().top;
-  var elBottom = elTop + elHeight;
+  const elTop = el.getBoundingClientRect().top;
+  const elBottom = elTop + elHeight;
 
   // Get elements positions minus offsets
-  var eTop = elTop + elOffset;
-  var eBottom = elTop + elHeight - offsetTop;
+  const eTop = elTop + elOffset;
+  const eBottom = elTop + elHeight - offsetTop;
 
   // return true or false
-  return (eTop <= cBottom && eBottom >= elOffset);
+  return eTop <= cBottom && eBottom >= elOffset;
 };
 
-var checkVisibility = function() {
-  for (var section of sections) {
+const checkVisibility = () => {
+  for (const section of sections) {
     if (inView(section)) {
       section.classList.add('in-view');
     } else {
